2f6f8d8aac105737b1cc6d152e98771d04844b03
ArdenJ <j.gaddas@outlook.com>
2019-10-05 13:40:05 +0100

    Added event handler to display the correct number of Day instances

--- schema.js
+++ schema.js
@@ -8,24 +8,15 @@ const {
     GraphQLString,
     GraphQLBoolean,
     GraphQLList,
+    GraphQLID,
 } = require('graphql');
-
-
-// // Fake Type 
-// const FakeType = new GraphQLObjectType({
-//     name: 'FakeName',
-//     fields: () => ({
-//         id: { type: GraphQLString },
-//         name: { type: GraphQLString },
-//     })
-// });
 
 // CALENDAR TYPES
 // Events
 const EventType = new GraphQLObjectType({
     name: 'DayEvents',
     fields: () => ({
-        event_index: { type: GraphQLString },
+        event_index: { type: GraphQLID },
         event_title: { type: GraphQLString },
         event_des: { type: GraphQLString },
         event_complete: { type: GraphQLBoolean },
@@ -46,7 +37,7 @@ const TimeType = new GraphQLObjectType({
 })
 
 
-// Root Query 
+// Root Query / Resolver
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -59,20 +50,6 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 })
-
-// Root Query --- YO THIS MUST REFERENCE THE OTHER SHIT
-// const RootQuery = new GraphQLObjectType({
-//     name: 'RootQueryType',
-//     fields: {
-//         FAKE: {
-//             type: new GraphQLList(FakeType),
-//             resolve(_, args) {
-//                 return axios.get('http://localhost:3001/fakeDatabase')
-//                     .then(res => res.data)
-//             },
-//         }
-//     }
-// })
 
 module.exports = new GraphQLSchema({
     query: RootQuery
